bf100d9c11f7905d58595d2a295b556f4f664bc3
Julien Dargelos <julien.dargelos@me.com>
2016-09-19 02:06:08 +0200

    New content on the About page

--- app/View/page/about.php
+++ app/View/page/about.php
@@ -9,5 +9,35 @@
 				</p>
 			</div>
 		</section>
+		<section class="howitworks">
+			<h3>Comment ça fonctionne&nbsp;?</h3>
+			<article>
+				<div class="content">
+					<?= svg('satellys') ?>
+					<h4>Satellys</h4>
+					<p>
+						<span>Les données sont automatiquement récupérées sur le site de Satellys avec vos identifiants.</span>
+					</p>
+				</div>
+			</article>
+			<article>
+				<div class="content">
+					<?= svg('gears') ?>
+					<h4>API</h4>
+					<p>
+						<span>L'API BetterSched' interprète puis traîte les données pour les organiser de façon organisée.</span>
+					</p>
+				</div>
+			</article>
+			<article>
+				<div class="content">
+					<?= svg('tiles') ?>
+					<h4>Interface</h4>
+					<p>
+						<span>BetterSched' met en forme l'emploi du temps pour qu'il soit simple et lisible pour vous.</span>
+					</p>
+				</div>
+			</article>
+		</section>
 	</div>
 </main>
