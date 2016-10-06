5bfe6f8a0d95109f8a2d4a3c6e342e9bc5790e81
Julien Dargelos <julien.dargelos@me.com>
2016-10-06 17:43:39 +0200

    Quotes can be removed

--- app/Mailer/Quote.php
+++ app/Mailer/Quote.php
@@ -22,10 +22,16 @@
 			$message .= ' a posté une citation sur BetterSched\'';
 			$message .= '</p>';
 			$message .= '<blockquote>'.$quote->content.'</blockquote>';
-			$message .= '<a class="button" href="'.htmlentities('http://'.$_SERVER['HTTP_HOST'].Route::url('page', 'validateQuote', [
+			$message .= '<a class="button" href="'.htmlentities('http://'.$_SERVER['HTTP_HOST'].Route::url('page', 'moderateQuote', [
+				'action' => 'validate',
 				'id' => $quote->id,
 				'key' => $quote->key
-			])).'">Valider la citation</a>';
+			])).'">Valider la citation</a> ';
+			$message .= '<a class="button light" href="'.htmlentities('http://'.$_SERVER['HTTP_HOST'].Route::url('page', 'moderateQuote', [
+				'action' => 'remove',
+				'id' => $quote->id,
+				'key' => $quote->key
+			])).'">Supprimer la citation</a>';
 
 			$subject = 'Citation sur BetterSched\'';
 
@@ -37,13 +43,31 @@
 			return (new Mail(self::MODERATOR, $subject, $html))->send();
 		}
 
-		static public function notifyAuthor(\Model\Quote $quote) {
+		static public function notifyAuthorAboutValidation(\Model\Quote $quote) {
 			if(Mail::add($quote->email)) {
 				$message = '<p>Salut toi, ta citation sur BetterSched\' a été approuvée !</p>';
 				$message .= '<blockquote>'.$quote->content.'</blockquote>';
 				$message .= '<p>Elle sera postée bientôt, bises.</p>';
 
 				$subject = 'Citation approuvée sur BetterSched\'';
+
+				$html = self::template([
+					'subject' => $subject,
+					'message' => $message
+				]);
+
+				return (new Mail($quote->email, $subject, $html))->send();
+			}
+			else return false;
+		}
+
+		static public function notifyAuthorAboutDeletion(\Model\Quote $quote) {
+			if(Mail::add($quote->email)) {
+				$message = '<p>Salut toi, malheureusement ta citation sur BetterSched\' a été refusée...</p>';
+				$message .= '<blockquote>'.$quote->content.'</blockquote>';
+				$message .= '<p>Désolé, bises.</p>';
+
+				$subject = 'Citation refusée sur BetterSched\'';
 
 				$html = self::template([
 					'subject' => $subject,
