6ca6b820c81c94c52c86a7794e4795250ae7b166
Julien Dargelos <julien.dargelos@me.com>
2016-11-13 22:24:00 +0100

    Changes the donation ammount

--- app/View/partials/information.php
+++ app/View/partials/information.php
@@ -1,5 +1,5 @@
 <div class="information">
 	<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6WLVJMJ2NDUDW" data-external>
-		Soutenez la plateforme en donnant <span>10 centimes</span> pour financer le renouvellement du nom de domaine
+		Soutenez la plateforme en donnant <span>1 euro</span> pour financer le renouvellement du nom de domaine
 	</a>
 </div>
